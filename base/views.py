c6631186170d2cc4c1ed3145f8c6d6f92f301340
Nursultan02 <nur.bek.aru.bag@gmail.com>
2021-12-28 00:01:09 +0600

    creating model room and register that, superuser to access db

--- base/views.py
+++ base/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+rooms = [
+    {'id': 1, 'name': 'python'},
+    {'id': 2, 'name': 'frontend'},
+    {'id': 3, 'name': 'design'},
+]
 
-def room(request):
-    return render(request, 'room.html')
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == pk:
+            room = i
+    return render(request, 'base/room.html', {'room': room})
